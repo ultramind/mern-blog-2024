184b78e2e9a94eb78067d0856a8607fe7bbeda8e
Akachukwu <udechukwuakachukwu@gmail.com>
2024-06-15 23:45:44 +0100

    fixing the cookie issue

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = async (userId, res) => {
     maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'none'
+    sameSite: 'None'
   })
 }
 
